914e8ca5996fdd7dbd5e178be5b66ec4498b6faf
BrianBett125 <brianbett756@gmail.com>
2023-09-12 12:09:27 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,18 +1,24 @@
 #include "main.h"
-#include <stdio.h>
+	#include <stdio.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int _abs(int i)
-{
-	if (i >= 0)
+
+	/**
+	 * main - check the code
+	 *
+	 * Return: Always 0.
+	 */
+	int main(void)
 	{
-		return (i);
-	}
-	else
-	{
-		return (i * -1);
+	    int r;
+
+
+	    r = _abs(-1);
+	    printf("%d\n", r);
+	    r = _abs(0);
+	    printf("%d\n", r);
+	    r = _abs(1);
+	    printf("%d\n", r);
+	    r = _abs(-98);
+	    printf("%d\n", r);
+	    return (0);
 	}
