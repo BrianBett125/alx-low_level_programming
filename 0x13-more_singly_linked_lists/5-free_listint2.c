f1331b8d23f0def1c316e71d5c883ee3685e33c3
BrianBett125 <brianbett756@gmail.com>
2023-10-24 12:16:11 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,17 +6,15 @@
  */
 void free_listint2(listint_t **head)
 {
- listint_t *temp;
+	 listint_t *temp;
 
- if (head == NULL)
- return;
-
- while (*head)
- {
- temp = (*head)->next;
- free(*head);
- *head = temp;
- }
-
- *head = NULL;
+	 if (head == NULL)
+	 return;
+	 while (*head)
+	 {
+		  temp = (*head)->next;
+		  free(*head);
+		  *head = temp;
+	 }
+	 *head = NULL;
 }
